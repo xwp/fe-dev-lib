bda720d466faf5c54f459359aaefca9e4ca8d6ca
Piotr Delawski <piotr.delawski@xwp.co>
2017-07-26 19:56:01 +0200

    Update build code.

--- dist/utils/pre-check.js
+++ dist/utils/pre-check.js
@@ -9,22 +9,12 @@ var _gulpUtil = require('gulp-util');
 
 var _gulpUtil2 = _interopRequireDefault(_gulpUtil);
 
-var _validateNodeVersion = require('validate-node-version');
-
-var _validateNodeVersion2 = _interopRequireDefault(_validateNodeVersion);
-
 var _getConfig = require('./get-config');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var preCheck = exports.preCheck = function preCheck() {
-	var nodeTest = (0, _validateNodeVersion2.default)(),
-	    exitCode = 1;
-
-	if (!nodeTest.satisfies) {
-		_gulpUtil2.default.log(_gulpUtil2.default.colors.red(nodeTest.message));
-		process.exit(exitCode);
-	}
+	var exitCode = 1;
 
 	if (undefined === _getConfig.workflow) {
 		_gulpUtil2.default.log(_gulpUtil2.default.colors.red('No workflow provided, aborting!'));
