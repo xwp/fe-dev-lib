49b6aa32390d35b9f02f66973d3d9036915500c8
Piotr Delawski <piotr.delawski@xwp.co>
2017-07-26 19:52:40 +0200

    Remove `validate-node-version` since Node version is already validated on install.

--- src/utils/pre-check.js
+++ src/utils/pre-check.js
@@ -1,15 +1,8 @@
 import gutil from 'gulp-util';
-import validateNode from 'validate-node-version';
 import { workflow, env } from './get-config';
 
 export const preCheck = function() {
-	const nodeTest = validateNode(),
-		exitCode = 1;
-
-	if ( ! nodeTest.satisfies ) {
-		gutil.log( gutil.colors.red( nodeTest.message ) );
-		process.exit( exitCode );
-	}
+	const exitCode = 1;
 
 	if ( undefined === workflow ) {
 		gutil.log( gutil.colors.red( `No workflow provided, aborting!` ) );
