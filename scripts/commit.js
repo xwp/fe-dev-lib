17d93707fe0a78f4c635f711e773168f09f74287
Justin Kopepasah <justin@kopepasah.com>
2017-09-12 13:15:43 +0900

    Fix eslint errors in commit.js.

--- scripts/commit.js
+++ scripts/commit.js
@@ -5,16 +5,16 @@
 const { exec, exit, echo, env } = require( 'shelljs' );
 
 // Check for changes to commit.
-if ( '' === exec( 'git status dist --porcelain', { silent: true }).stdout ) {
+if ( '' === exec( 'git status dist --porcelain', { silent: true } ).stdout ) {
 	echo( 'No changes in dist to commit.' );
 	exit( 0 );
 }
 
-if ( exec( 'git commit -m ":zap: babel compile (bot) [skip ci]" -- dist' ).code !== 0 ) {
+if ( 0 !== exec( 'git commit -m ":zap: babel compile (bot) [skip ci]" -- dist' ).code ) {
 	echo( 'Error: Git commit failed' );
 	exit( 0 );
 }
 
-if ( 'true' === env['TRAVIS'] ) {
+if ( 'true' === env['TRAVIS'] ) { // eslint-disable-line dot-notation
 	exec( 'git push' );
 }
